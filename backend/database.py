be93e6d1465dc0e43fedaa8aeb9d449eae543d8a
parsanithin5 <pparsanithin@gmail.com>
2026-03-13 22:25:38 +0530

    Fix MongoDB TLS connection

--- backend/database.py
+++ backend/database.py
@@ -1,20 +1,6 @@
-from pymongo import MongoClient
-
-MONGO_URL = "mongodb+srv://zerotrustadmin:ZeroTrust123@cluster0.ebhkf16.mongodb.net/?appName=Cluster0"
-
-client = MongoClient(MONGO_URL)
-
-db = client["zero_trust_db"]
-
-users_collection = db["users"]
-behavior_collection = db["behavior_logs"]
-risk_collection = db["risk_reports"]
-admin_notifications = db["admin_notifications"]
-
 # from pymongo import MongoClient
-# import os
 
-# MONGO_URL = os.getenv("MONGO_URL")
+# MONGO_URL = "mongodb+srv://zerotrustadmin:ZeroTrust123@cluster0.ebhkf16.mongodb.net/?appName=Cluster0"
 
 # client = MongoClient(MONGO_URL)
 
@@ -24,3 +10,23 @@ admin_notifications = db["admin_notifications"]
 # behavior_collection = db["behavior_logs"]
 # risk_collection = db["risk_reports"]
 # admin_notifications = db["admin_notifications"]
+
+
+from pymongo import MongoClient
+import certifi
+import os
+
+MONGO_URL = os.getenv("MONGO_URL")
+
+client = MongoClient(
+    MONGO_URL,
+    tls=True,
+    tlsCAFile=certifi.where()
+)
+
+db = client["zero_trust_db"]
+
+users_collection = db["users"]
+behavior_collection = db["behavior_logs"]
+risk_collection = db["risk_reports"]
+admin_notifications = db["admin_notifications"]
