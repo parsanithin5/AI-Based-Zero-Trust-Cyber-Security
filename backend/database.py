71f2ef1cbae6380c3e6c6e5ebf11bfc264e18329
parsanithin5 <pparsanithin@gmail.com>
2026-03-13 21:10:08 +0530

    Fix MongoDB connection for Render

--- backend/database.py
+++ backend/database.py
@@ -1,15 +1,7 @@
-# from pymongo import MongoClient
-
-# client = MongoClient("mongodb://localhost:27017")
-# db = client["zero_trust_db"]
-
-# users_collection = db["users"]
-# behavior_collection = db["behavior_logs"]
-# risk_collection = db["risk_reports"]
-# admin_notifications = db["admin_notifications"]
 from pymongo import MongoClient
+import os
 
-MONGO_URL = "mongodb+srv://zerotrustadmin:ZeroTrust123@cluster0.ebhkf16.mongodb.net/?appName=Cluster0"
+MONGO_URL = os.getenv("MONGO_URL")
 
 client = MongoClient(MONGO_URL)
 
